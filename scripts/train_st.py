c8ef4aaf9022ee6fe1e7e94976745e5d4cf3508f
Roberto Garcia <betogarcia97@live.com.mx>
2025-11-08 15:17:57 -0600

    Improving training with sentence-transformers

--- scripts/train_st.py
+++ scripts/train_st.py
@@ -1,57 +1,95 @@
+import sys
+import logging
+
 import dotenv
 dotenv.load_dotenv()
 
 import torch.distributed as dist
 from torch.distributed import get_rank, is_initialized
-from sentence_transformers import SentenceTransformer, losses, SentenceTransformerTrainer, SentenceTransformerTrainingArguments
+from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, SentenceTransformerTrainingArguments
+from sentence_transformers.losses import CachedMultipleNegativesRankingLoss
+from sentence_transformers.training_args import BatchSamplers
+from sentence_transformers.evaluation import InformationRetrievalEvaluator
 from datasets import load_dataset, Dataset
 
-MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
+DEFAULT_MODEL = 'all-MiniLM-L6-v2'
 DATASET_NAME = 'Rivert97/ug-normativity'
-SAVE_MODEL = './Rivert97/all-MiniLM-L6-v2-ug-normativity'
+SAVE_MODEL = './Rivert97/'
 BATCH_SIZE = 16
 EPOCHS = 2
-WARMUP_STEPS = 10
+
+# Set the log level to INFO to get more information
+logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
+
+# You can specify any Hugging Face pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
+model_name = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_MODEL
+model_name_only = model_name.split("/")[-1]
 
 def main():
     # 1. Load base model
-    model = SentenceTransformer(MODEL_ID)
+    model = SentenceTransformer(model_name)
 
-    # 2. load training dataset
+    # 2. Load training dataset
     dataset = load_dataset(DATASET_NAME, split='train')
-    print(dataset)
-
-    train_anchors = [data['question'] for data in dataset]
-    train_positives = [data['context_text'] for data in dataset]
-    train_dataset = Dataset.from_dict({
-        "anchor": train_anchors,
-        "positive": train_positives,
-    })
+    dataset_dict = dataset.train_test_split(test_size=0.1, seed=12)
+    train_dataset: Dataset = Dataset.from_dict({"anchor": dataset_dict["train"]["question"], "positive": [a['text'][0] for a in dataset_dict["train"]["answers"]]})
+    eval_dataset: Dataset = Dataset.from_dict({"id": dataset_dict["test"]["id"], "context": dataset_dict["test"]["context_text"], "question": dataset_dict["test"]["question"], "answer": [a['text'][0] for a in dataset_dict["test"]["answers"]]})
+    print(train_dataset)
+    print(eval_dataset)
 
     # 3. Define a loss function
-    # Use MultipleNegativesRankingLoss for contrastive fine-tuning
-    train_loss = losses.MultipleNegativesRankingLoss(model)
+    train_loss = CachedMultipleNegativesRankingLoss(model, mini_batch_size=32)
 
     # 4. Define training arguments
+    run_name = f"{model_name_only}-ug-normativity"
     args = SentenceTransformerTrainingArguments(
+        # Required parameter:
+        output_dir=f"models/{run_name}",
+        # Optional training parameters:
         num_train_epochs=EPOCHS,
         per_device_train_batch_size=BATCH_SIZE,
-        warmup_steps=WARMUP_STEPS,
-        fp16=True,
-        dataloader_drop_last=True,
+        per_device_eval_batch_size=BATCH_SIZE,
+        learning_rate=2e-5,
+        warmup_ratio=0.1,
+        fp16=False,  # Set to False if you get an error that your GPU can't run on FP16
+        bf16=True,  # Set to True if you have a GPU that supports BF16
+        batch_sampler=BatchSamplers.NO_DUPLICATES,  # MultipleNegativesRankingLoss benefits from no duplicate samples in a batch
+        # Optional tracking/debugging parameters:
+        eval_strategy="steps",
+        eval_steps=100,
+        save_strategy="steps",
+        save_steps=100,
+        save_total_limit=2,
+        logging_steps=25,
+        logging_first_step=True,
     )
 
-    # 5. Train
+    # 5. Create an evaluator & evaluate the base model
+    print("Initial evaluation")
+    dev_evaluator = InformationRetrievalEvaluator(
+        queries={q["id"]: q["question"] for q in eval_dataset},
+        corpus={"Doc_"+q["id"]: q["context"] for q in eval_dataset},
+        relevant_docs={q["id"]:["Doc_"+q["id"]] for q in eval_dataset},
+        batch_size=BATCH_SIZE,
+        name="sts-dev",
+    )
+    dev_evaluator(model)
+
+    # 6. Train
     trainer = SentenceTransformerTrainer(
         model=model,
         args=args,
         train_dataset=train_dataset,
+        eval_dataset=Dataset.from_dict({"anchor": dataset_dict["test"]["question"], "positive": [a['text'][0] for a in dataset_dict["test"]["answers"]]}),
         loss=train_loss,
     )
     trainer.train()
 
+    print("Final evaluation")
+    dev_evaluator(model)
+
     if not is_initialized() or get_rank() == 0:
-        trainer.model.save(SAVE_MODEL)
+        trainer.model.save(f"{SAVE_MODEL}/{run_name}")
 
     if dist.is_initialized():
         dist.destroy_process_group()
