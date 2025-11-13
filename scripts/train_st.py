6c0c074efe21eba4a023ba23a30b95b69ec89b42
Roberto Garcia <betogarcia97@live.com.mx>
2025-11-13 12:59:23 -0600

    Adding option for triplet training

--- scripts/train_st.py
+++ scripts/train_st.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import random
 
 import dotenv
 dotenv.load_dotenv()
@@ -10,13 +11,15 @@ from sentence_transformers import SentenceTransformer, SentenceTransformerTraine
 from sentence_transformers.losses import CachedMultipleNegativesRankingLoss
 from sentence_transformers.training_args import BatchSamplers
 from sentence_transformers.evaluation import InformationRetrievalEvaluator
+from transformers import EarlyStoppingCallback
 from datasets import load_dataset, Dataset
 
 DEFAULT_MODEL = 'all-MiniLM-L6-v2'
 DATASET_NAME = 'Rivert97/ug-normativity'
 SAVE_MODEL = './Rivert97/'
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 EPOCHS = 2
+TRIPLETS = True
 
 # Set the log level to INFO to get more information
 logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
@@ -32,14 +35,20 @@ def main():
     # 2. Load training dataset
     dataset = load_dataset(DATASET_NAME, split='train')
     dataset_dict = dataset.train_test_split(test_size=0.1, seed=12)
-    train_dataset: Dataset = Dataset.from_dict({"anchor": dataset_dict["train"]["question"], "positive": dataset_dict["train"]["context_text"]})
+    if TRIPLETS:
+        train_dataset: Dataset = Dataset.from_dict({"anchor": dataset_dict["train"]["question"], "positive": dataset_dict["train"]["context_text"], "negative": random.sample(dataset_dict["train"]["context_text"], len(dataset_dict["train"]["context_text"]))})
+    else:
+        train_dataset: Dataset = Dataset.from_dict({"anchor": dataset_dict["train"]["question"], "positive": dataset_dict["train"]["context_text"]})
     print(train_dataset)
 
     # 3. Define a loss function
     train_loss = CachedMultipleNegativesRankingLoss(model, mini_batch_size=32)
 
     # 4. Define training arguments
-    run_name = f"{model_name_only}-ug-normativity"
+    if TRIPLETS:
+        run_name = f"{model_name_only}-ug-normativity-triplets"
+    else:
+        run_name = f"{model_name_only}-ug-normativity"
     args = SentenceTransformerTrainingArguments(
         # Required parameter:
         output_dir=f"models/{run_name}",
@@ -54,12 +63,15 @@ def main():
         batch_sampler=BatchSamplers.NO_DUPLICATES,  # MultipleNegativesRankingLoss benefits from no duplicate samples in a batch
         # Optional tracking/debugging parameters:
         eval_strategy="steps",
-        eval_steps=100,
+        eval_steps=50,
         save_strategy="steps",
-        save_steps=100,
+        save_steps=50,
         save_total_limit=2,
         logging_steps=25,
         logging_first_step=True,
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_loss",
+        greater_is_better=False,
     )
 
     # 5. Create an evaluator & evaluate the base model
@@ -78,8 +90,9 @@ def main():
         model=model,
         args=args,
         train_dataset=train_dataset,
-        eval_dataset=Dataset.from_dict({"anchor": dataset_dict["test"]["question"], "positive": dataset_dict["test"]["context_text"]}),
+        eval_dataset=Dataset.from_dict({"anchor": dataset_dict["test"]["question"], "positive": dataset_dict["test"]["context_text"], "negative": random.sample(dataset_dict["test"]["context_text"], len(dataset_dict["test"]["context_text"]))}),
         loss=train_loss,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
     )
     trainer.train()
 
